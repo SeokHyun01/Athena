992911eb7969cc6ee01d3cb131c8eaa5e1955a20
SeokHyun Youn <cheongyeon2018@gmail.com>
2023-02-21 13:23:54 +0900

    버그 수정

--- Athena_DataAccess/Migrations/20230221042309_SetEventHeaderPathNullable.Designer.cs
+++ Athena_DataAccess/Migrations/20230221042309_SetEventHeaderPathNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Athena_DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AthenaDataAccess.Migrations
 {
     [DbContext(typeof(AthenaAppDbContext))]
-    partial class AthenaAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230221042309_SetEventHeaderPathNullable")]
+    partial class SetEventHeaderPathNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
