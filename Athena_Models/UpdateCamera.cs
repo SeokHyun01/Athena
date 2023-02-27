c77ad3e8c9d5a979b0dbe0053d9edf64497ca8ac
SeokHyun Youn <cheongyeon2018@gmail.com>
2023-02-27 16:52:40 +0900

    버그 수정

--- Athena_Models/UpdateCamera.cs
+++ Athena_Models/UpdateCamera.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Athena_Models
 {
 	public class UpdateCamera
 	{
+		[Required]
 		public int CameraId { get; set; }
 		public string? Thumbnail { get; set; }
 		public int? Degree { get; set; }
