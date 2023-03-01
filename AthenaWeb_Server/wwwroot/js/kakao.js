a7eb4f25ca537df11671ae50d7e78a7d4d017a8b
Yurve <https://github.com/Yurve>
2023-03-01 18:50:12 +0900

    test

--- AthenaWeb_Server/wwwroot/js/kakao.js
+++ AthenaWeb_Server/wwwroot/js/kakao.js
@@ -1,50 +1,59 @@
-window.kakaoLogin = () => {
+window.kakaoLogin = async () => {
+    try {
+        Kakao.init('8743aa8ae98410121d5eb950d8d9fe56');
 
-    Kakao.init('8743aa8ae98410121d5eb950d8d9fe56');
-    
-    Kakao.Auth.login({
-        success: function (response) {
+        await new Promise((resolve, reject) => {
+            Kakao.Auth.login({
+                success: function (response) {
+                    resolve();
+                },
+                fail: function (error) {
+                    reject(error);
+                },
+            })
+        });
+
+        let response = await new Promise((resolve, reject) => {
             Kakao.API.request({
                 url: '/v2/user/me',
                 success: function (response) {
-                    console.log(response.id, response.kakao_account.profile.nickname)
+                    resolve(response);
                 },
                 fail: function (error) {
-                    console.log(error)
+                    reject(error);
                 },
             })
-        },
-        fail: function (error) {
-            console.log(error)
-        },
-    })
+        });
 
-    if(Kakao.Auth.getAccessToken()) {
+        console.log(response.id, response.kakao_account.profile.nickname);
         return true;
-    } else {
+    } catch (error) {
+        console.log(error);
         return false;
     }
 }
 
-window.kakaoLogout = () => {
 
-    if (Kakao.Auth.getAccessToken()) {
-        Kakao.API.request({
-            url: '/v1/user/unlink',
-            success: function (response) {
-                console.log(response)
-            },
-            fail: function (error) {
-                console.log(error)
-            },
-        })
-        Kakao.Auth.setAccessToken(undefined)
-    }
+window.kakaoLogout = async () => {
+    try {
+        if (Kakao.Auth.getAccessToken()) {
+            await new Promise((resolve, reject) => {
+                Kakao.API.request({
+                    url: '/v1/user/unlink',
+                    success: function (response) {
+                        resolve(response);
+                    },
+                    fail: function (error) {
+                        reject(error);
+                    },
+                })
+            });
+            Kakao.Auth.setAccessToken(undefined);
+        }
 
-    if(Kakao.Auth.getAccessToken()) {
-        return true;
-    } else {
+        return !Kakao.Auth.getAccessToken();
+    } catch (error) {
+        console.log(error);
         return false;
     }
 }
-
