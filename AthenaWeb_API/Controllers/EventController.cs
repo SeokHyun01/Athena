167d9cc3821d493346b7918d3897eec73a5540f6
SeokHyun Youn <cheongyeon2018@gmail.com>
2023-03-06 00:27:27 +0900

    JSON Serialize 버그 수정

--- AthenaWeb_API/Controllers/EventController.cs
+++ AthenaWeb_API/Controllers/EventController.cs
@@ -46,7 +46,7 @@ namespace AthenaWeb_API.Controllers
 					if (eventObj.EventHeader.IsRequiredObjectDetection)
 					{
 						_logger.LogInformation(eventObj.EventHeader.Path);
-						var content = JsonConvert.SerializeObject(eventObj.EventHeader);
+						var content = JsonConvert.SerializeObject(eventObj);
 						_logger.LogInformation(content);
 						var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
 						var response = await _client.PostAsync("http://localhost:8000/", bodyContent);
