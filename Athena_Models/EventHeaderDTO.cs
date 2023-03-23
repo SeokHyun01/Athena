1585e19fc6095c35dd2875ddeede78b3a47801f6
SeokHyun Youn <cheongyeon2018@gmail.com>
2023-03-23 10:56:39 +0900

    Event api request 모델 수정

--- Athena_Models/EventHeaderDTO.cs
+++ Athena_Models/EventHeaderDTO.cs
@@ -16,8 +16,8 @@ namespace Athena_Models
 		public string? Path { get; set; }
 		public bool IsRequiredObjectDetection { get; set; }
 		public int CameraId { get; set; }
-		public Camera Camera { get; set; }
-		public IEnumerable<EventBody> EventBodies { get; set; }
+		public Camera? Camera { get; set; }
+		public IEnumerable<EventBody>? EventBodies { get; set; }
 		public int EventVideoId { get; set; }
 	}
 }
