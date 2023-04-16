7fe0c5ab50a83e4e86881bae15f36b9e1e5c8767
Yurve <https://github.com/Yurve>
2023-04-16 14:35:16 +0900

    test

--- AthenaWeb_Server/Hubs/SignalingHub.cs
+++ AthenaWeb_Server/Hubs/SignalingHub.cs
@@ -88,6 +88,7 @@ namespace AthenaWeb_Server.Hubs
 			var roomNames = Rooms.Where(x => x.Value.Contains(senderId)).Select(x => x.Key);
 			foreach (var roomName in roomNames)
 			{
+				_logger.LogInformation(ice, " ICE를 전송합니다.");
 				var group = Clients.GroupExcept(roomName, senderId);
 				await group.SendAsync("ReceiveIce", ice);
 				_logger.LogInformation($"방 {roomName}으로 ICE를 전송했습니다.");
