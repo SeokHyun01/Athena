c39c138f83282f5c2656834609f1dd8265f7f4a6
Yurve <https://github.com/Yurve>
2023-04-17 00:19:22 +0900

    test

--- AthenaWeb_Server/wwwroot/js/rtcvideochat.js
+++ AthenaWeb_Server/wwwroot/js/rtcvideochat.js
@@ -137,12 +137,15 @@ class Camera {
     
     // 안드로이드에서는 stream 메서드가 삭제됨 각각의 track을 가져와야함
     handleAddTrack(data) {
-        if (data && data.streams[0]) {
+        if (data && data.streams) {
             console.log("handleAddTrack 비디오 가져오기");
             this.remoteVideo = document.getElementById("remoteVideo");
             this.remoteVideo.srcObject = data.streams[0];
             //좌우 반전
             this.remoteVideo.style.transform = "scaleX(-1)";
+        }else{
+            console.log(data);
+            console.log(data.streams[0])
         }
     }
 
