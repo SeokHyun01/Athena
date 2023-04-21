1ab6add981e2c08206926f79d6e8a0f7cca67a50
Yurve <https://github.com/Yurve>
2023-04-21 13:25:56 +0900

    test

--- AthenaWeb_Server/wwwroot/js/rtcvideochat.js
+++ AthenaWeb_Server/wwwroot/js/rtcvideochat.js
@@ -235,7 +235,7 @@ function receiveAnswer(answer) {
 
 function receiveIce(ice) {
     const receivedIce = JSON.parse(ice);
-
+    console.log(ice.data.candidate);
     camera.peerConnection.addIceCandidate(receivedIce)
     .then(() => {
         console.log("IceCandidate added successfully");
