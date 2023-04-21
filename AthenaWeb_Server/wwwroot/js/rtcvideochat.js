3e0f0ed7e7c7deba9c87f06bfa2d099143f65dbc
Yurve <https://github.com/Yurve>
2023-04-21 16:59:45 +0900

    Test

--- AthenaWeb_Server/wwwroot/js/rtcvideochat.js
+++ AthenaWeb_Server/wwwroot/js/rtcvideochat.js
@@ -140,7 +140,7 @@ class Camera {
     handleAddTrack(data) {
         if (data && data.streams) {
             const stream = new MediaStream();
-            console.log(data)
+            console.log("data", data)
             for (const track of data.streams[0].getTracks()) {
                 if (track.kind === "video" || track.kind === "audio") {
                     stream.addTrack(track);
